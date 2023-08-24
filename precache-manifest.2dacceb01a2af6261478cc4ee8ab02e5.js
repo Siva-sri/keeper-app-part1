5e3aa58213ed2be3205565404eb5b42dd53ac68a
Rubasri S <rubasrisrikanthan@gmail.com>
2023-08-24 19:58:30 +0530

    Updates

---
+++ precache-manifest.2dacceb01a2af6261478cc4ee8ab02e5.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d98538b85a957bf2aa6f73c4a0472a0a",
+    "url": "/keeper-app-part1/index.html"
+  },
+  {
+    "revision": "8b959ee2a6658c0d0299",
+    "url": "/keeper-app-part1/static/js/2.235e61d4.chunk.js"
+  },
+  {
+    "revision": "07a0533b005bf0b29307",
+    "url": "/keeper-app-part1/static/js/main.18ff4de8.chunk.js"
+  },
+  {
+    "revision": "21e28ec7dcd0f381bfc7",
+    "url": "/keeper-app-part1/static/js/runtime-main.902ad6f4.js"
+  }
+]);
